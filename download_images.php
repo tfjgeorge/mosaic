dc59dd4798324c506b19681ae2f1b8b34e66206a
Mehdi Boubaker <mehdi.boubaker+git@gmail.com>
2012-10-20 02:10:37 +0200

    afichage rgb

--- download_images.php
+++ download_images.php
@@ -1,13 +1,22 @@
 <?php
 
-$videos =json_decode(file_get_contents("https://api.dailymotion.com/videos?search=obama&limit=100&fields=filmstrip_small_url,id"));
+$videos = json_decode(file_get_contents("https://api.dailymotion.com/videos?search=obama&limit=100&fields=filmstrip_small_url,id"));
 foreach ($videos->{'list'} as $video) {
 	if (!empty($video->{'filmstrip_small_url'})) {
 		$image = imagecreatefromjpeg($video->{'filmstrip_small_url'});
 		$monopixel = imagecreatetruecolor(1, 1);
 
 		imagecopyresized($monopixel, $image, 0, 0, 0, 0, 1, 1, 160, 120);
-		print_r(imagecolorat($monopixel, 0, 0));
+		$rgb = imagecolorat($monopixel, 0, 0);
+		echo "r= ";
+		$r = ($rgb >> 16) & 0xFF;
+		echo $r;
+		echo "\ng= ";
+		$g = ($rgb >> 8) & 0xFF;
+		echo $g;
+		echo "\nb= ";
+		$b = $rgb & 0xFF;
+		echo $b;
 		echo "\n";
 	}
 } 
