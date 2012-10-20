f803cdb095e0a49b726aeb1e8a6c1faa2c702e60
Thomas George <tfjgeorge@gmail.com>
2012-10-20 21:34:06 +0200

    available colors avec sqlite3

--- available_colors.php
+++ available_colors.php
@@ -1,8 +1,9 @@
 <?php
 
-$sqlite = sqlite_open('base.sqlite');
+$sqlite = new SQLite3('base.sqlite');
+$result = $sqlite->query('SELECT r, g, b FROM images;');
 
-foreach(sqlite_array_query($sqlite,'SELECT r, g, b FROM images;') as $record) {
+while($record = $result->fetchArray()) {
 	printf ("<div style=\"background-color: rgb(%s, %s, %s); height: 20; width: 20; display: inline-block;\" ></div>", $record['r'],$record['g'],$record['b'] );
 }
 
