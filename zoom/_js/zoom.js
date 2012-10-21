c707d8ce567dcb5238f1821c148c8e8fd7f81580
Thomas George <tfjgeorge@gmail.com>
2012-10-21 05:50:34 +0200

    layout in 3 part for the website

---
+++ zoom/_js/zoom.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    var cont_left = $("#content").position().left;
+    var jsonTab = new Array();
+
+    function place_elements() {
+        var height = parseInt($('#content').css('height'));
+        var ratio = parseInt($('#img_zoomed').css('width')) / parseInt($('#img_zoomed').css('height'));
+        var width = parseInt(height / (3/4 + 1/ratio));
+        $('#full_image').css({width: width});
+        $('#zoomed_image').css({left: width});
+
+        var zoomed_div_height = parseInt($('#zoomed_image').css('height'));
+        var zoomed_div_width = parseInt($('#zoomed_image').css('width'));
+        $('#cadre').css({'margin-left': -10000 + zoomed_div_width / 2 - 48, 'margin-top': -10000 + zoomed_div_height / 2 - 48});
+
+        $('#thumb').css({top:parseInt($('#full_image img').css('height')),width: width})
+        $('#thumb_img').css({top:parseInt((parseInt($('#thumb').css('height'))-120)/2),left: parseInt((parseInt($('#thumb').css('width'))-160)/2)})
+    }
+
+    $(window).resize(function(){ place_elements(); });
+
+    $('#img_coord').mousemove(function(e) {
+        var x = e.pageX - this.offsetLeft;
+        var y = e.pageY - this.offsetTop - 47;
+
+        var zoomed_div_height = parseInt($('#zoomed_image').css('height'));
+        var zoomed_div_width = parseInt($('#zoomed_image').css('width'));
+
+        var zoomed_image_height = parseInt($('#img_zoomed').css('height'));
+        var zoomed_image_width = parseInt($('#img_zoomed').css('width'));
+
+        var small_image_height = parseInt($('#img_coord').css('height'));
+        var small_image_width = parseInt($('#img_coord').css('width'));
+
+        $('#img_zoomed').css({'margin-top':zoomed_div_height / 2 - y / small_image_height * zoomed_image_height,'margin-left': zoomed_div_width / 2 - x / small_image_width * zoomed_image_width });
+        var video = jsonTab[parseInt(y / small_image_height *  zoomed_image_height / 32)][parseInt(x / small_image_width *  zoomed_image_width / 32)];
+        $('#thumb img').attr('src',video['image_url']);
+        $('#thumb img').css('margin-top',-video['position']*120);
+
+    });
+
+    $('#img_coord').click(function(e) {
+        var x = e.pageX - this.offsetLeft;
+        var y = e.pageY - this.offsetTop - 47;
+
+        var zoomed_image_height = parseInt($('#img_zoomed').css('height'));
+        var zoomed_image_width = parseInt($('#img_zoomed').css('width'));
+
+        var small_image_height = parseInt($('#img_coord').css('height'));
+        var small_image_width = parseInt($('#img_coord').css('width'));
+        
+        var video = jsonTab[parseInt(y / small_image_height *  zoomed_image_height / 32)][parseInt(x / small_image_width *  zoomed_image_width / 32)];
+
+        $.fancybox({href: "http://www.dailymotion.com/embed/video/"+video['video_id']+"?autoPlay=1", type: 'iframe', closeEffect: 'none'});
+    });
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "output.json",
+        success: function(data) {
+           jsonTab = data;
+        }
+    });
+
+    setTimeout(place_elements,500);
+    
+});
---
+++ zoom/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--CSS-->
+        <link rel="stylesheet" href="_css/style.css" />
+        <link rel="stylesheet" href="fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+        
+        <!--JS-->
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+        <script type="text/javascript" src="_js/zoom.js"></script>
+        <script type="text/javascript" src="fancybox/jquery.fancybox.pack.js"></script>
+        
+        <!--META-->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            
+    </head>
+    <body>
+        <div id="top_header_expanded">
+            <div id="top_header">
+                <h1>Mozaïk</h1>
+                <input class="search" name="search" type="search" placeholder="Entrez un mot-clef" />
+                <input class="submit_search" name="submit_search" type="submit" />
+            </div>
+        </div>
+        <div id="content">
+            <div id="full_image">
+                <img id="img_coord" src="output.jpeg" />
+            </div>
+            <div id="zoomed_image">
+                <img id="img_zoomed" src="output.jpeg" />
+                <div id="cadre"></div>
+            </div>
+            <div id="thumb">
+                <div id="thumb_img">
+                    <img />
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
