a2d7d727bfe2873fdd747cdaa7364d5d29fb25e2
Thomas George <tfjgeorge@gmail.com>
2012-10-21 10:11:36 +0200

    release version

---
+++ release/download_images.php
@@ -0,0 +1,52 @@
+<?php
+
+
+$search = $_POST['search'];
+// echo $search;
+ 
+/* GESTION FICHIER ENVOYE */
+
+$_FILES['fichier']['name'];     //Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_fichier.png).
+$_FILES['fichier']['type'] ;    //Le type du fichier. Par exemple, cela peut être « image/png ».
+$_FILES['fichier']['size']  ;   //La taille du fichier en octets.
+$_FILES['fichier']['tmp_name']; //L'adresse vers le fichier uploadé dans le répertoire temporaire.
+$_FILES['fichier']['error'];    //Le code d'erreur, qui permet de savoir si le fichier a bien été uploadé.
+if ($_FILES['fichier']['error'] > 0) $erreur = "Erreur lors du transfert";
+$extensions_valides = array('jpg', 'jpeg');
+$extension_upload = strtolower(substr(strrchr($_FILES['fichier']['name'], '.')  ,1)  );
+
+$sqlite = new SQLite3('base.sqlite');
+$sqlite->query('DROP TABLE images;');
+$sqlite->query('CREATE TABLE images (r int, g int, b int, video_id string, image_url string, position int);');
+
+for ($page=1; $page < 11; $page++) { 
+	$videos = json_decode(file_get_contents("https://api.dailymotion.com/videos?search=".mysql_real_escape_string(htmlspecialchars($search))."&limit=100&page=$page&fields=filmstrip_small_url,id"));
+
+	foreach ($videos->{'list'} as $video) {
+		if (!empty($video->{'filmstrip_small_url'})) {
+			$image = imagecreatefromjpeg($video->{'filmstrip_small_url'});
+			imagejpeg($image,'images/'.urlencode($video->{'filmstrip_small_url'}));
+
+			$monopixel = imagecreatetruecolor(1, 1);
+
+			for ($i=0; $i<8; $i++) {
+				$thumb = imagecreatetruecolor(160, 120);
+				imagecopyresized($thumb, $image, 0, 0, 0, $i*120, 160, 120, 160, 120);
+				imagecopyresampled($monopixel, $image, 0, 0, 0, $i*120, 1, 1, 160, 120);
+				$rgb = imagecolorat($monopixel, 0, 0);
+				$r = ($rgb >> 16) & 0xFF;
+				$g = ($rgb >> 8) & 0xFF;
+				$b = $rgb & 0xFF;
+				// echo "$r|$g|$b => ".$video->{'filmstrip_small_url'};
+				// echo "\n";
+				$sqlite->query("INSERT INTO images (r, g, b, video_id, image_url, position) VALUES ($r, $g, $b, '".$video->{'id'}."', '".$video->{'filmstrip_small_url'}."', $i)");
+			}
+		}
+	}
+}
+
+include_once('generate_mosaic.php');
+header('Location: index.php');
+
+
+?>
---
+++ release/generate_mosaic.php
@@ -0,0 +1,69 @@
+<?php
+
+header ('Content-Type: image/jpeg');
+set_time_limit(0);
+
+$tolerance = 20;
+$min_width = 64;
+$sqlite = new SQLite3('base.sqlite'); 
+
+$input_raw = imagecreatefromjpeg($_FILES['fichier']['tmp_name']);
+// resample image
+list($image_width, $image_height) = getimagesize($_FILES['fichier']['tmp_name']);
+if ($image_width > $image_height) {
+	$width = $min_width;
+	$height = (int) ($min_width * $image_height / $image_width);
+}
+else {
+	$height = $min_width;
+	$width = (int) ($min_width * $image_width / $image_height);
+}
+$input = imagecreatetruecolor($width, $height);
+imagecopyresampled($input, $input_raw, 0, 0, 0, 0, $width, $height, $image_width, $image_height);
+
+$output = imagecreatetruecolor($width*32, $height*32);
+$map = array();
+
+for ($y=0; $y < $height; $y++) {
+	$map[$y] = array();
+	for ($x=0; $x < $width; $x++) {
+
+		// current pixel
+		$rgb = imagecolorat($input, $x, $y);
+		$r = ($rgb >> 16) & 0xFF;
+		$g = ($rgb >> 8) & 0xFF;
+		$b = $rgb & 0xFF;
+
+		$closest_colors = $sqlite->query('SELECT r, g, b, image_url, video_id, position FROM images WHERE '.
+			'r <= '.($r+$tolerance).' AND r >= '.($r-$tolerance).
+			' AND g <= '.($g+$tolerance).' AND g >= '.($g-$tolerance).
+			' AND b <= '.($b+$tolerance).' AND b >= '.($b-$tolerance).';');
+		
+		$results = array();
+		while ($current = $closest_colors->fetchArray()) {
+			if(!isset($current['image_url'])) continue; 
+			array_push($results,$current);
+		}
+		$color = $results[array_rand($results)];
+
+		if ($color) {
+			$image = imagecreatefromjpeg('images/'.urlencode($color['image_url']));
+			$map[$y][$x] = array('video_id' => $color['video_id'], 'image_url' => $color['image_url'], 'position' => $color['position']);
+
+			imagecopyresized($output, $image, $x*32, $y*32, 0, $color['position']*120, 32, 32, 160, 120);
+		}
+		else {
+			for ($i=0; $i<32; $i++) {
+				for ($j=0; $j<32; $j++) {
+					imagesetpixel($output, $x*32+$i, $y*32+$j, $rgb);
+				}
+			}
+		}
+
+	}
+}
+
+imagejpeg($output,'output.jpeg');
+file_put_contents('output.json', json_encode($map));
+
+?>
